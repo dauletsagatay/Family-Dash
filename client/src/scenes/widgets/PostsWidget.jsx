8730d7373b9c905c809a60116efb1fdfeb348eb1
Daulet Sagatay <d.sagatay@imbc.kz>
2022-12-22 10:30:50 +0600

    first commit

---
+++ client/src/scenes/widgets/PostsWidget.jsx
@@ -0,0 +1,81 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setPosts } from "state";
+import PostWidget from "./PostWidget";
+
+const PostsWidget = ({ userId, isProfile = false }) => {
+  const dispatch = useDispatch();
+  const posts = useSelector((state) => state.posts);
+  const token = useSelector((state) => state.token);
+
+  const getPosts = async () => {
+    const response = await fetch(`${process.env.REACT_APP_BASE_URL}/posts`, {
+      method: "GET",
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    const data = await response.json();
+    data.sort((p1, p2) => {
+      return new Date(p2.createdAt) - new Date(p1.createdAt);
+    });
+    dispatch(setPosts({ posts: data }));
+  };
+
+  const getUserPosts = async () => {
+    const response = await fetch(
+      `${process.env.REACT_APP_BASE_URL}/posts/${userId}/posts`,
+      {
+        method: "GET",
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    );
+    const data = await response.json();
+    data.sort((p1, p2) => {
+      return new Date(p2.createdAt) - new Date(p1.createdAt);
+    });
+    dispatch(setPosts({ posts: data }));
+  };
+
+  useEffect(() => {
+    if (isProfile) {
+      getUserPosts();
+    } else {
+      getPosts();
+    }
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  return (
+    <>
+      {posts.map(
+        ({
+          _id,
+          userId,
+          firstName,
+          lastName,
+          description,
+          location,
+          picturePath,
+          userPicturePath,
+          likes,
+          comments,
+          createdAt,
+        }) => (
+          <PostWidget
+            key={_id}
+            postId={_id}
+            postUserId={userId}
+            name={`${firstName} ${lastName}`}
+            description={description}
+            location={location}
+            picturePath={picturePath}
+            userPicturePath={userPicturePath}
+            likes={likes}
+            comments={comments}
+            createdAt={createdAt}
+          />
+        )
+      )}
+    </>
+  );
+};
+
+export default PostsWidget;
